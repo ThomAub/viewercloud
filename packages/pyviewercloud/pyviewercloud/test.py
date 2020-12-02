5cafb85fc09d9b6f0e9eeb78d8685b8c7987c485
Thomas Aubry <github.thomaub@gmail.com>
2020-12-03 00:09:33 +0100

    :art: change api and now use add_pointcloud add_centroid

--- packages/pyviewercloud/pyviewercloud/test.py
+++ packages/pyviewercloud/pyviewercloud/test.py
@@ -1,29 +1,34 @@
 import numpy as np
 import pyviewercloud as pcviewer
 
-
-def test_viewer_small_pointcloud():
-    viewer = pcviewer.PointcloudViewer(120, 120, 15)
-    small_point_cloud = np.random.randn(5000, 3).astype(np.float32)
-    # viewer.plot_3d_points([small_point_cloud], [[255, 255, 255]])
-    # assert True
-
-
-def test_viewer_small_red_small_blue():
-    viewer = pcviewer.PointcloudViewer(120, 120, 15)
-    small_red = np.random.randn(5000, 3).astype(np.float32)
-    small_blue = np.random.randn(5000, 3).astype(np.float32)
-    viewer.plot_3d_points([small_red, small_blue], [[255, 0, 0], [0, 0, 255]])
+def test_viewer_kitti_pointcloud():
+    # viewer = pcviewer.PointcloudViewer(1200, 1800, 15000)
+    # kitti_point_cloud = np.fromfile("../../tests/data/kitti/velodyne/000001.bin", dtype=np.float32).reshape((-1, 4))[:,:3]
+    # viewer.add_pointcloud(kitti_point_cloud, [255, 255, 255])
+    # viewer.show()
     assert True
 
+def test_viewer_lyft_pointcloud():
+    # # New viewer
+    # viewer = pcviewer.PointcloudViewer(1200, 1800, 15000)
+    # # Load some pointcloud from the lyft perception dataset
+    # lyft_point_cloud_1 = np.fromfile("../../tests/data/lyft/host-a101_lidar0_1241893239502712366.bin", dtype=np.float32).reshape((-1, 5))[:,:3]
+    # lyft_point_cloud_2= np.fromfile("../../tests/data/lyft/host-a101_lidar1_1241893239502712366.bin", dtype=np.float32).reshape((-1, 5))[:,:3]
+    # lyft_point_cloud_3= np.fromfile("../../tests/data/lyft/host-a101_lidar2_1241893239502712366.bin", dtype=np.float32).reshape((-1, 5))[:,:3]
+    # # Add them one by one to the viewer to have different color
+    # viewer.add_pointcloud(lyft_point_cloud_1, [255, 0, 0])
+    # viewer.add_pointcloud(lyft_point_cloud_2, [0, 0, 255])
+    # viewer.add_pointcloud(lyft_point_cloud_3, [0, 255, 0])
+    # # You can now display the window
+    # viewer.show()
+    assert True
 
-def test_viewer_medium_pointcloud():
-    viewer = pcviewer.PointcloudViewer(120, 120, 15)
-    medium_point_cloud = np.random.randn(5000, 3).astype(np.float32)
-    # viewer.plot_3d_points(medium_point_cloud, [255, 255, 255])
-
-
-def test_viewer_large_pointcloud():
-    viewer = pcviewer.PointcloudViewer(120, 120, 15)
-    large_point_cloud = np.random.randn(5000, 3).astype(np.float32)
-    # viewer.plot_3d_points(large_point_cloud, [255, 255, 255])
+def test_viewer_kitti_with_centroid():
+    # viewer = pcviewer.PointcloudViewer(1200, 1800, 15000)
+    # kitti_point_cloud = np.fromfile("../../tests/data/kitti/velodyne/000001.bin", dtype=np.float32).reshape((-1, 4))[:,:3]
+    # viewer.add_pointcloud(kitti_point_cloud, [255, 255, 255])
+    # #centroids = np.random.randn(3, 3).astype(np.float32)
+    # centroids = np.array([[-11.5,0,-0.8]]).astype(np.float32)
+    # viewer.add_centroid(centroids, [255, 0, 0])
+    # viewer.show()
+    assert True
--- packages/pyviewercloud/src/lib.rs
+++ packages/pyviewercloud/src/lib.rs
@@ -1,99 +1,160 @@
 use kiss3d::light::Light;
 use kiss3d::resource::{AllocationType, BufferType, GPUVec};
 use kiss3d::window::Window;
+use na::geometry::Translation3;
 use na::Point3;
 use nalgebra as na;
-use ndarray::{stack, Array, Array2, Axis};
+use ndarray::{stack, Array, Array2, ArrayView2, Axis};
 use numpy::PyReadonlyArray2;
 use pyo3::prelude::{pyclass, pymethods, pymodule, PyModule, PyResult, Python};
 use pyo3::types::PyList;
 
 use viewercloud::renderer::PointCloudRenderer;
 use viewercloud::viewer::AppState;
+use viewercloud::PointCloud;
 /// PointcloudViewer Class
+/// window_height: u32,
+/// window_width: u32,
+/// max_points: usize,
+/// colored_pointcloud: PointCloud,
+/// centroids: Vec<(Point3<f32>, Point3<f32>)>,
+/// debug: bool,
 #[pyclass(unsendable)]
 struct PointcloudViewer {
     window_height: u32,
     window_width: u32,
     max_points: usize,
+    colored_pointcloud: PointCloud,
+    centroids: Vec<(Point3<f32>, Point3<f32>)>,
     debug: bool,
 }
 #[pymethods]
 impl PointcloudViewer {
+    /// Create an new instance of viewercloud
     #[new]
-    fn new(window_height: u32, window_width: u32, max_points: usize) -> Self {
+    pub fn new(window_height: u32, window_width: u32, max_points: usize) -> Self {
         PointcloudViewer {
             window_height,
             window_width,
             max_points,
+            colored_pointcloud: PointCloud::new(),
+            centroids: vec![],
             debug: false,
         }
     }
-    fn plot_3d_points(&self, pointclouds: &PyList, pointclouds_color: &PyList) {
-        let pointclouds: Vec<PyReadonlyArray2<f32>> = pointclouds.extract().expect("Cannot extract pointcloud list");
-        let pointclouds_color: Vec<Vec<f32>> = pointclouds_color.extract().expect("Cannot extract color list list");
-        let mut data: Vec<Point3<f32>> = vec![];
-        if pointclouds.len() != pointclouds_color.len() {
-            panic!(
-                "The number of pointcloud and colors should be the same. Here found {} pointclouds and {} colors",
-                pointclouds.len(),
-                pointclouds_color.len()
-            )
-        }
-        let pts_colored = pointclouds.iter().zip(pointclouds_color.iter());
-
-        for (pts_3d, color) in pts_colored {
-            // looking at the number of points in the pointcloud
-            let pts_3d = pts_3d.as_array();
-            let n_points = pts_3d.raw_dim()[0];
-
-            if self.debug {
-                if n_points < self.max_points {
-                    println!("Creating window with {} points", n_points);
-                } else {
-                    println!(
-                        "Creating window with {} points instead of {} for performance",
-                        self.max_points, n_points
-                    );
-                }
-            }
-            if self.debug {
+    /// Add a new pointcloud and an associated color to the current viewer.
+    ///
+    /// Args: pointcloud: numpy.ndarray [NX3] in float32 each row represent a point.
+    ///       color: List[int, int, int] List storing the RGB representation of the color.
+    pub fn add_pointcloud(&mut self, pointcloud: PyReadonlyArray2<f32>, color: &PyList) {
+        let pointcloud: ArrayView2<f32> = pointcloud.as_array();
+        let color: Vec<f32> = color.extract().expect("Cannot extract list to plot the pointcloud");
+        let n_points = pointcloud.raw_dim()[0];
+        if self.debug {
+            if n_points < self.max_points {
+                println!("Creating window with {} points", n_points);
+            } else {
                 println!(
-                    "Display 1 Pointcloud is the color ({},{},{})",
-                    color[0], //.get_item(0),
-                    color[1], //.get_item(1),
-                    color[2]  //.get_item(2)
+                    "Creating window with {} points instead of {} for performance",
+                    self.max_points, n_points
                 );
             }
-            let color_vec: Vec<f32> = color.to_vec();
-            let color_base: Array2<f32> = ndarray::ArrayBase::ones((n_points, 3));
-            let mul_color = Array::from(color_vec);
-            let final_color = color_base * mul_color.broadcast((n_points, 3)).expect("Cannot brodcast color");
-
-            let points = stack!(Axis(1), pts_3d, final_color)
-                .into_shape((n_points * 2, 3))
-                .expect("Cannot stack data together");
-
-            let mut points_vec: Vec<Point3<f32>> = points
-                .axis_chunks_iter(Axis(0), 1)
-                .map(|vec_own| vec_own.to_slice().unwrap())
-                .map(|slice| Point3::from_slice(slice))
-                .collect();
-
-            data.append(&mut points_vec);
         }
+        if self.debug {
+            println!(
+                "Display 1 Pointcloud with the color ({},{},{})",
+                color[0], //.get_item(0),
+                color[1], //.get_item(1),
+                color[2]  //.get_item(2)
+            );
+        }
+        let color_base: Array2<f32> = ndarray::ArrayBase::ones((n_points, 3));
+        let mul_color = Array::from(color);
+        let final_color = color_base * mul_color.broadcast((n_points, 3)).expect("Cannot brodcast color");
+
+        let points = stack!(Axis(1), pointcloud, final_color)
+            .into_shape((n_points * 2, 3))
+            .expect("Cannot stack data together");
+
+        let mut points_vec: Vec<Point3<f32>> = points
+            .axis_chunks_iter(Axis(0), 1)
+            .map(|vec_own| vec_own.to_slice().unwrap())
+            .map(|slice| Point3::from_slice(slice))
+            .collect();
+
+        self.colored_pointcloud.append(&mut points_vec);
+    }
+    /// Add a new centroid with the associated color to the current viewer.
+    /// The centroid is represented as an sphere of radius 0.1 m
+    ///
+    /// Args: centroids: numpy.ndarray [NX3] in float32 each row represent a centroid.
+    ///       color: List[int, int, int] List storing the RGB representation of the color.
+    pub fn add_centroid(&mut self, centroids: PyReadonlyArray2<f32>, color: &PyList) {
+        let centroids: ArrayView2<f32> = centroids.as_array();
+        let color: Vec<f32> = color.extract().expect("Cannot extract color list");
+        let n_points = centroids.raw_dim()[0];
+        if self.debug {
+            if n_points < self.max_points {
+                println!("Creating window with {} points", n_points);
+            } else {
+                println!(
+                    "Creating window with {} points instead of {} for performance",
+                    self.max_points, n_points
+                );
+            }
+        }
+        if self.debug {
+            println!(
+                "Display 1 centroid with the color ({},{},{})",
+                color[0], //.get_item(0),
+                color[1], //.get_item(1),
+                color[2]  //.get_item(2)
+            );
+        }
+        let mut points_vec: Vec<(Point3<f32>, Point3<f32>)> = centroids
+            .axis_chunks_iter(Axis(0), 1)
+            .map(|vec_own| vec_own.to_slice().unwrap())
+            .inspect(|x| {
+                dbg!(x);
+            })
+            .map(|slice| (Point3::from_slice(slice), Point3::from_slice(&color)))
+            .collect();
+        self.centroids.append(&mut points_vec);
+    }
+    pub fn show(&self) {
         let mut window = Window::new_with_size("Viewercloud", self.window_height, self.window_width);
         window.set_light(Light::StickToCamera);
-
-        let point_cloud_data: GPUVec<Point3<f32>> = GPUVec::new(data, BufferType::Array, AllocationType::StreamDraw);
+        let pointcloud_gpu: GPUVec<Point3<f32>> = GPUVec::new(
+            self.colored_pointcloud.data.clone(),
+            BufferType::Array,
+            AllocationType::StreamDraw,
+        );
+        for (centroid, color) in self.centroids.iter() {
+            let mut c = window.add_sphere(0.1);
+            c.set_color(color.x, color.y, color.z);
+            c.prepend_to_local_translation(&Translation3::new(centroid.x, centroid.y, centroid.z));
+        }
+        if self.debug {
+            println!("The points are loaded in GPU {}", pointcloud_gpu.is_on_gpu());
+        }
         let app = AppState {
-            point_cloud_renderer: PointCloudRenderer::new(2.0, point_cloud_data),
+            point_cloud_renderer: PointCloudRenderer::new(2.0, pointcloud_gpu),
         };
         window.render_loop(app);
     }
 }
 
-/// Main entry of the python module
+/// Main entry of the python module pyviewercloud
+/// Contains the class PointcloudViewer
+///
+/// ```python
+/// import pyviewercloud
+/// viewer = pyviewercloud.PointcloudViewer(120, 120, 15)
+/// pointcloud = np.random.randn(50000, 3).astype(np.float32)
+/// viewer.add_pointcloud(pointcloud, [255, 255, 255])
+/// centroids = np.random.randn(3, 3).astype(np.float32)
+/// viewer.add_centroid(centroids, [255, 0, 0])
+/// ```
 #[pymodule]
 fn pyviewercloud(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_class::<PointcloudViewer>()?;
